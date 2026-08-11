b7c8b3b70c53e9d7f3e2452e7efd3d8a91703c1a
Jeremiah-E <jkevans2006@gmail.com>
2026-08-11 16:48:19 -0500

    Improve command handling and change output

--- setup.ps1
+++ setup.ps1
@@ -2,8 +2,9 @@
 # If you pass -F, skip SDL configure/build for a faster compilation.
 # Use this only when SDL or the build configuration hasn't changed.
 
+# Our flags
 param(
-    [switch]$F
+    [switch]$F # "Fast" mode, skips SDL configuration
 )
 
 # Filepaths we need to keep track of
@@ -11,69 +12,97 @@ $SDLRoot = Join-Path $env:USERPROFILE "vendored/SDL"
 $SDLBuild = Join-Path $SDLRoot "build"
 $SDLInstall = Join-Path $SDLRoot "install"
 
-# Check if SDL exists
-if (!(Test-Path -Path $SDLRoot)) {
-    $command = {git clone https://github.com/libsdl-org/SDL.git $SDLRoot}
-    $message = "Install SDL"
-    # See the foreach block near the end for comments on this
-    $output = & $command 2>&1
-    if ($LASTEXITCODE -eq 0) {
-        Write-Host "[Success] -" $message -ForegroundColor Green
-    } else {
-        Write-Host "[Failure] -" $message -ForegroundColor Red
-        $output
-        exit 1
-    }
+# Success/failure characters for compatibility
+$checkmark = "$([char]0x2713)" # https://www.compart.com/en/unicode/U+2713
+$skipmark = "-"
+$crossmark = "$([char]0x2717)" # https://www.compart.com/en/unicode/U+2717
+# Fallbacks if Unicode doesn't work
+$unicodeCheck = [Console]::OutputEncoding.CodePage -eq 65001
+if (!$unicodeCheck) {
+    $checkmark = "+"
+    $skipmark = "-"
+    $crossmark = "x"
 }
 
+# Commands to run
 $commands = @(
     @{
+        command = {git clone https://github.com/libsdl-org/SDL.git $SDLRoot};
+        message = "Install SDL";
+        tests = @("SDL Installed", "Fast Flag")
+    }
+    @{
         command = {cmake -S $SDLRoot -B $SDLBuild};
-        message = "Configure SDL source"
+        message = "Configure SDL Source";
+        tests = @("Fast Flag")
     }
     @{
         command = {cmake --build $SDLBuild --config Release };
-        message = "Build SDL with CMake"
+        message = "Build SDL with CMake";
+        tests = @("Fast Flag")
     }
     @{
         command = {cmake --install $SDLBuild --prefix $SDLInstall};
-        message = "Install SDL to vendored/install"
+        message = "Install SDL to vendored/install";
+        tests = @("Fast Flag")
     }
     @{
         command = {cmake -S . -B build};
-        message = "Configure project build"
+        message = "Configure Project Build";
+        tests = @()
     }
     @{
         command = {cmake --build build};
-        message = "Build project"
+        message = "Build Project";
+        tests = @()
     }
     @{
         command = {Copy-Item .\build\Debug\NBody.exe . ; Copy-Item $SDLRoot\install\bin\SDL3.dll .};
-        message = "Copy executable and lib to current root"
+        message = "Copy .exe and .dll to Root";
+        tests = @()
     }
 )
-
-# If we give it the F flag, skip two commands
-if ($F) {
-    $commands = $commands[2..($commands.Count - 1)]
-}
 
 # Run each command, breaking if encountering an error
 foreach ($instruction in $commands) {
     $command = $instruction.command
     $message = $instruction.message
+    $tests = $instruction.tests
 
-    # Run the command, storing it
-    $output = & $command 2>&1
-
-    # If no error happened, print the message
-    if ($LASTEXITCODE -eq 0) {
-        Write-Host "[Success] -" $message -ForegroundColor Green
+    # Check for various conditions
+    $doCommand = $true;
+    $reason = ""
+    foreach ($test in $tests) {
+        if ($test -eq "SDL Installed") {
+            $SDLExists = Test-Path -Path $SDLRoot
+            $doCommand = $doCommand -and !$SDLExists
+        } elseif ($test -eq "Fast Flag") {
+            $doCommand = $doCommand -and !$F
+        } else {
+            Write-Host "Unrecognized condition: $test"
+            exit
+        }
+        if (!$doCommand) {
+            $reason = $test
+            break
+        }
     }
-    # Otherwise, print the message *and* the output
-    else {
-        Write-Host "[Failure] -" $message -ForegroundColor Red
-        $output
-        exit 1
+
+    if ($doCommand) {
+        # Run the command, storing its output to $output
+        $output = & $command 2>&1
+    
+        # If no error happened, print the short description of the command
+        if ($LASTEXITCODE -eq 0) {
+            Write-Host "[$checkmark] - $message" -ForegroundColor Green
+        }
+        # Otherwise, print the description *and* the output of the command
+        else {
+            Write-Host "[$crossmark] - $message" -ForegroundColor Red
+            $output
+            exit 1
+        }
+    } else {
+        Write-Host "[$skipmark] - Skipped $message - $reason" -ForegroundColor DarkGray
     }
 }
